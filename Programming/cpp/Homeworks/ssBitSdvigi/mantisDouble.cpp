83c12a77e1f6bc6d1efab19f420b384ace7e543b
calexzz <calexzz@MacBook-Pro-calexzz.local>
2025-11-11 10:07:22 +0800

    new files

--- Programming/cpp/Homeworks/ssBitSdvigi/mantisDouble.cpp
+++ Programming/cpp/Homeworks/ssBitSdvigi/mantisDouble.cpp
@@ -6,62 +6,29 @@ typedef unsigned long long int ulli;
 
 int main(void) {
     setlocale(LC_ALL, "Russian");
-    double f = 2.5;
+    double f = -7.9;
     long long int n = *((long long int*) &f);
     cout << "Число " << f << " в 16-ричном виде: " << hex << n << endl << endl;
 
-    ulli sign = (n >> 63) & 0x1;
-    ulli exp = (n >> 52) & 0x7FF;
-    ulli mantissa = n & 0x7FFFFFFFFFFFF;
-
-    cout << "Знак: " << sign << endl;
-    cout << "Экспонента: " << exp << endl;
-    cout << "Мантисса: " << mantissa << endl;
-    cout << endl;
-
-    exp -= 1023;
-    cout << "Экспонента: " << exp << endl;
+    int sign = (n >> 63) & 0x1 ? -1 : 1;
 
     ulli one = 1;
     ulli m = n & ((one << 52) - 1);
-
     ulli e = ((n>>52) & 0x7FF) - 1023;
 
-    cout << "Экспонента: " << e << endl;
+    cout << "Знак: " << dec<< sign << endl;
+    cout << "Экспонента: " << dec << e << endl;
+    cout << "Мантисса: " << dec << m << endl;
+    cout << endl;
 
-    double new_f;
+    m += (one << 52);//учитываем единицу
 
-//    unsigned long long int new_n = (sign << 63) | (exp << 52) | mantissa;
-//    double new_f = *((double*)&new_n);
+    ulli cel = m >> (52 - e);
+    ulli lli_drob = m & ((one << (52 - e)) - 1);
+    double d_drob = (double)lli_drob / (one << (52 - e));
 
-//    cout << "Восстановленное число: " << new_f << endl;
-
-//#include <iostream>
-//#include <locale.h>
-//    using namespace std;
-//
-//    int main(void)
-//    {
-//        setlocale(LC_ALL, "rus");
-//        float f = 2.5;
-//        int n = *((int*)&f);
-//        int ex = (n & 0x7F800000);
-//        int mant = (n & 0x007FFFFF);
-//        mant += 0x00800000;
-//        /*cout << ex << endl;*/
-//        ex = (ex >> 23)-127;
-//        /*cout << ex << endl;*/
-//        int cel = mant >> 23-ex;
-//        /*cout << hex <<cel  << endl;*/
-//        int mask = (1 << (23 - ex)) - 1;
-//        int fractional_bits = mant & mask;
-//        float drob = (float)fractional_bits / (1 << (23 - ex));
-//        cout << "Целая часть: " << cel << endl;
-//        cout << "Дробная часть: " << drob << endl;
-//        cout << "Исходное число: " << f << endl;
-//        cout << "Восстановленное: " << (cel + drob) << endl;
-//        return 0;
-//    }
+    double new_f = sign * (cel + d_drob);
+    cout<<new_f<<endl;
 
     return 0;
 }
--- Programming/cpp/Homeworks/ssBitSdvigi/mantisExp.cpp
+++ Programming/cpp/Homeworks/ssBitSdvigi/mantisExp.cpp
@@ -2,23 +2,38 @@
 #include <locale.h>
 using namespace std;
 
+
 int main(void) {
     setlocale(LC_ALL, "Russian");
     float f = -2.5;
     int n = *((int *) &f);
     cout << "Число " << f << " в 16-ричном виде: " << hex << n << endl << endl;
 
-    unsigned int sign = (n >> 31) & 0x1;
-    unsigned int exp = (n >> 23) & 0xFF;
-    unsigned int mantissa = n & 0x7FFFFF;
+    int sign = (n >> 31) & 0x1 ? -1 : 1;
+    int exp = (n >> 23) & 0xFF;
+    int mantissa = n & 0x7FFFFF;
 
     cout << "Знак: " << dec << sign << endl;
     cout << "Экспонента: " << dec << exp << endl;
     cout << "Мантисса: " << dec << mantissa << endl;
     cout << endl;
 
-    unsigned int new_n = (sign << 31) | (exp << 23) | mantissa;
-    float new_f = *((float*)&new_n);
+    exp -= 127;
+    mantissa += 0x800000; //учли единичку
+    int cel = mantissa >> (23-exp);
+    int i_drob = mantissa & ((1<<(23-exp))-1);
+
+    float f_drob = (float)i_drob / (1 << (23-exp));
+    float new_f = sign * (cel + f_drob);
+
+    //    for (int i = 31; i>=0; i--){
+//        unsigned int bit = (n >> i) & 1;
+//        cout<<bit;
+//
+//        if (i == 31 || i == 23)
+//            cout<<" ";
+//    }
+//    cout<<endl<<endl;
 
     cout << "Восстановленное число: " << new_f << endl;
 
