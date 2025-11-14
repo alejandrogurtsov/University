806be78bf0f6da172bb9397376f30eb1f6762908
alejandrogurtsov <alex.1046296@gmail.com>
2025-11-14 15:44:19 +0800

    task 2 programmers and troll

--- Programming/cpp/Lessons/November/lesson02/main.cpp
+++ Programming/cpp/Lessons/November/lesson02/main.cpp
@@ -7,9 +7,11 @@ using namespace std;
 int main(void)
 {
     setlocale(LC_ALL,"RU");
+    srand(time(NULL));
+
     int board[8][8]={0};
 
-    //заполнили доску моентками
+    //заполнили доску монетками
     for (int i = 0; i<8; i++){
         for (int j = 0; j<8; j++){
             board[i][j] = rand() % 2;
@@ -22,16 +24,15 @@ int main(void)
         }
         cout<<endl;
     }
-    cout<<endl;
 
-
-    unsigned int ish_board=0;
+    //четность исходной доски
+    int ish_board=0;
     for (int bits = 0; bits < 6; bits++) {
         int count = 0;
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 int cell = i * 8 + j;
-                if (((cell >> bits) & 1) && (board[i][j] == 0))
+                if (((cell >> bits) & 1) && (board[i][j] == 1))
                     count+=1;
             }
         }
@@ -40,8 +41,45 @@ int main(void)
         }
     }
 
-    cout<<ish_board<<endl;
+    for(int i=5; i>=0; i--){
+        int bit = (ish_board >> i) & 1;
+        cout<<bit;
+    }
+    cout<<" - четность исходной доски"<<endl;
 
+    //тролль выбирает монету
+    int tr_row, tr_col = 0;
+    cout<<"Введите номер загаданной монеты в формате - строка колонка (нумерация с 0):"<<endl;
+    cin>>tr_row>>tr_col;
+    int tr_cell = tr_row * 8 + tr_col;
+
+//xor-им исходную четность доски и загаданную троллем монету => получаем номер монеты которую перевернёт 1-ый программист
+    int cell_num = tr_cell ^ ish_board;
+
+//первый программист переворачивает монету
+    board[cell_num/8][cell_num%8] ^= 1;
+
+//второй программист находит четность новой доски что и является номером загаданной монеты
+    int isk_moneta=0;
+    for (int bits = 0; bits < 6; bits++) {
+        int count = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                int cell = i * 8 + j;
+                if (((cell >> bits) & 1) && (board[i][j] == 1))
+                    count+=1;
+            }
+        }
+        if (count % 2 == 1) {
+            isk_moneta = isk_moneta | (1 << bits);
+        }
+    }
+
+    for(int i=5; i>=0; i--){
+        int bit = (isk_moneta >> i) & 1;
+        cout<<bit;
+    }
+    cout<<" - номер загаданной монеты"<<endl;
 
     return 0;
 }
