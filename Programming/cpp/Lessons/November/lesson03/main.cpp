d161b1c2c4ebebc3bf6adcc6048179184dba4f67
alejandrogurtsov <alex.1046296@gmail.com>
2025-11-17 16:35:30 +0800

    new files

---
+++ Programming/cpp/Lessons/November/lesson03/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+const int FIB_NUMBER=10;
+int* createFib(void)
+{
+    int* arr = new int[FIB_NUMBER];
+    arr[0]=arr[1]=1;
+    for (int i=2; i<FIB_NUMBER; i++){
+        arr[i]=arr[i-1]+arr[i-2];
+    }
+    return arr;
+}
+
+int main(void) {
+    setlocale(LC_ALL,"RU");
+    int* fibArr = createFib();
+    cout<<"Числа Фибоначчи"<<endl;
+    for (int i = 0; i<FIB_NUMBER; i++){
+        cout<<fibArr[i]<<" ";
+    }
+    cout<<endl;
+    delete[] fibArr;
+
+    return 0;
+}
